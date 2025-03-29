6dcbed817ece9d5c6974604040ee5e4b47d7d2d0
nehsolanki436 <nehsolanki436@gmail.com>
2025-03-29 18:23:43 +0530

    Episode 3 completed

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
+const ele=<span className="fa fa-warning"> Hello!!</span>
 //JSX=>Babel Transpilation converts to React.createElement => ReactElement-JS Object => HTMLElement(render)
-const Title=()=><h1 className="heading" tabIndex="5">Namaste React!!</h1>;
+const Title=()=>(<h1 className="heading" tabIndex="5">{ele}Namaste React!!
+</h1>);
 
 
 const number = 100;
 //This is called component composition
-const HeadingComponent =() =>{
+var HeadingComponent =() =>{
     return <div id="container">
-     <Title />
+    <Title/>    
     <h1>{100+200}{console.log('hello')}</h1>
     <h1 className="headingComponent">Namaste React Functional Component!</h1>
     </div>
